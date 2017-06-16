ab547e5ed785799ed9a50a027f07ad5f04b81b10
Siddarth KM <siddarth.km@samsung.com>
2017-06-16 14:34:14 +0900

    Adding Start Message; git push origin master
    Signed-off-by: Siddarth KM <siddarth.km@samsung.com>

--- LUIS-Bot-Sample/Dialogs/RootDialog.cs
+++ LUIS-Bot-Sample/Dialogs/RootDialog.cs
@@ -14,6 +14,9 @@ namespace LUIS_Bot_Sample.Dialogs
     {
         public Task StartAsync(IDialogContext context)
         {
+            var task = context.PostAsync($"Hello I'm Tizen LUIS. How may I serve you.");
+            task.Wait();
+
             context.Wait(MessageReceivedAsync);
 
             return Task.CompletedTask;
@@ -26,7 +29,7 @@ namespace LUIS_Bot_Sample.Dialogs
             // calculate something for us to return
             int length = (activity.Text ?? string.Empty).Length;
 
-            await context.PostAsync($"Starting Luis query");
+            //await context.PostAsync($"Starting Luis query");
 
             try
             {
@@ -34,7 +37,7 @@ namespace LUIS_Bot_Sample.Dialogs
                 LuisService luis = new LuisService(new LuisModelAttribute("86684636-488d-48b3-a4c6-233ef496d3d1", "5f3fca65f0a64d68ab6d4d474b1b0fa6"));
                 var luisResult = await luis.QueryAsync(activity.Text, System.Threading.CancellationToken.None);
 
-                await context.PostAsync($"Luis Result received .. Processing intents");
+                //await context.PostAsync($"Luis Result received .. Processing intents");
 
                 switch (luisResult.TopScoringIntent.Intent)
                 {
@@ -43,7 +46,7 @@ namespace LUIS_Bot_Sample.Dialogs
                         await context.PostAsync($"Please repeat your query. I did not understand...");
                         break;
                     case "Greeting":
-                        await context.PostAsync($"Greetings.. How may I serve you?");
+                        await context.PostAsync($"Greetings to you too. Anything I can do for you?");
                         break;
                     default:
                         await context.PostAsync($"Please repeat your query. I could not recognize your intent...");
@@ -56,7 +59,7 @@ namespace LUIS_Bot_Sample.Dialogs
             }
 
             // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
             context.Wait(MessageReceivedAsync);
             //await context.Forward(new RootLuisDialog(), MessageReceivedAsync, activity, System.Threading.CancellationToken.None);
         }
